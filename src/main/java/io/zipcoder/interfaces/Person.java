986088c1316dc8f0a9c1eb9ee75b2a8af78a9e17
Theresa <theresa@Gbrads-Mac.home>
2021-03-27 22:18:41 -0400

    Completd 1.0 & 1.1

--- src/main/java/io/zipcoder/interfaces/Person.java
+++ src/main/java/io/zipcoder/interfaces/Person.java
@@ -2,4 +2,23 @@ package io.zipcoder.interfaces;
 
 public class Person {
 
+    private final long id;
+    private String name;
+
+    public Person(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/test/java/io/zipcoder/interfaces/TestPerson.java
+++ src/test/java/io/zipcoder/interfaces/TestPerson.java
@@ -1,5 +1,34 @@
 package io.zipcoder.interfaces;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestPerson {
 
+    @Test
+    public void constructorTest() {
+        // Given
+            Person p = new Person(12345, "Theresa");
+
+        // When
+        long actualId = p.getId();
+        String actualName = p.getName();
+
+        // Then
+        Assert.assertEquals(12345, actualId);
+        Assert.assertEquals("Theresa", actualName);
+    }
+
+    @Test
+    public void setNameTest() {
+        // Given
+        Person p = new Person(12345, "Theresa");
+
+        // When
+        p.setName("George");
+        String actual = p.getName();
+
+        // Then
+        Assert.assertEquals("George", actual);
+    }
 }
